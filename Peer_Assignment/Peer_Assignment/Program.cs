321cc6a296bd688a158d413f7082eff97837870e
Olivia Gauthier <org1181@rit.edu>
2024-10-15 21:13:19 -0400

    started partner a method

--- Peer_Assignment/Peer_Assignment/Program.cs
+++ Peer_Assignment/Peer_Assignment/Program.cs
@@ -5,9 +5,18 @@
         static void Main(string[] args)
         {
             //variables
-            int livFavoriteNumber = 42;
+            int livFavoriteNumber;
+
+            //initialize
+            livFavoriteNumber = 42;
 
             //start of our actual program
+            Console.WriteLine(livFavoriteNumber);
+        }
+
+        static void Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
