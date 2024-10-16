7e14a3cd76c5cc79775e935834c0ed96de671535
Olivia Croteau <oliviacroteau667@gmail.com>
2024-10-15 21:20:15 -0400

    added Subtract function

--- Peer_Assignment/Peer_Assignment/Program.cs
+++ Peer_Assignment/Peer_Assignment/Program.cs
@@ -6,8 +6,16 @@
         {
             //variables
             int livFavoriteNumber = 42;
+            int olliFavoriteNumber = 667;
+
+            static int Subtract(int x, int y)
+            {
+                return x - y;
+            }
 
             //start of our actual program
+
+
         }
     }
 }
