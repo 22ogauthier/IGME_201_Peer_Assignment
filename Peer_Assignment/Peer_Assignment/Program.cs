7b4d399c05383bb2fec154858313fa86b202b63d
Olivia Gauthier <org1181@rit.edu>
2024-10-15 21:09:04 -0400

    Initial creation

---
+++ Peer_Assignment/Peer_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Peer_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int livFavoriteNumber = 42;
+
+            //start of our actual program
+        }
+    }
+}
